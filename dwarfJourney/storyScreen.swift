d7d1bafc24cec83070b253fbf186ae39c2dbac6a
David <ehley001@rangers.uwp.edu>
2018-04-03 18:21:07 -0500

    Start screen image and button added. Constraints are done on the first screen. Labels and buttons added to stats screen. Constraints not added yet.

---
+++ dwarfJourney/storyScreen.swift
@@ -0,0 +1,26 @@
+//
+//  storyScreen.swift
+//  dwarfJourney
+//
+//  Created by Ehley, David A (ehley001) on 4/3/18.
+//  Copyright © 2018 Ehley, David A (ehley001). All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class storyScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
