93a3a39880d883d81f801d7a02b644e4eac38347
profile <ehley001@maclab-89.uwp.edu>
2018-04-11 14:54:46 -0500

    Linked combat and stats screen to the .swift files

--- dwarfJourney/combatScreen.swift
+++ dwarfJourney/combatScreen.swift
@@ -2,8 +2,27 @@
 //  combatScreen.swift
 //  dwarfJourney
 //
-//  Created by Ehley, David A (ehley001) on 4/10/18.
+//  Created by Ehley, David A (ehley001) on 4/11/18.
 //  Copyright © 2018 Ehley, David A (ehley001). All rights reserved.
 //
-
 import Foundation
+
+import UIKit
+
+class combatScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background1")!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
--- dwarfJourney/statScreen.swift
+++ dwarfJourney/statScreen.swift
@@ -5,5 +5,24 @@
 //  Created by Ehley, David A (ehley001) on 4/10/18.
 //  Copyright © 2018 Ehley, David A (ehley001). All rights reserved.
 //
-
 import Foundation
+
+import UIKit
+
+class statScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background1")!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
